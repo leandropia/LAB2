364f05225ef6d1ade345a3d45770b374ed7e09da
leandropia <leandropia@gmail.com>
2022-09-01 16:59:59 -0400

    Add project files.

---
+++ LAB2/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("*** Welcome to Lab 2 ***");
+        Console.WriteLine();
+
+        int num1, num2;
+
+        Console.WriteLine("Enter the first addend: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second addend: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+       int sum = num1 + num2;
+              
+        Console.WriteLine("The sum of the numbers entered is: "+ sum);
+
+      Console.ReadKey();
+    } }
